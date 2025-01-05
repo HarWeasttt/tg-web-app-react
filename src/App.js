fc93e96bda2220968bca7adbe7b37ca10a0aaa9b
https://github.com/HarWeasttt/PP.git <Andry_1203@mail.ru>
2025-01-05 13:22:44 +0300

    12

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
         <div className="App">
             <Header />
             <Routes>
-            <Route path={'form'} element={<Form />}/>
-                <Route index element={<ProductList />}/>
+            <Route index element={<Form />}/>
+                <Route path='product' element={<ProductList />}/>
             </Routes>
         </div>
     );
