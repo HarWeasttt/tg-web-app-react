33260643ce559e857752263dd5fcbaeabcd644a6
https://github.com/HarWeasttt/PP.git <Andry_1203@mail.ru>
2025-01-05 13:28:34 +0300

    16

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import {useTelegram} from "../../hooks/useTelegram";
+import { useTelegram } from "../../hooks/useTelegram";
+import { useNavigate } from 'react-router-dom'; // Импортируем useNavigate
 import './Header.css';
 import Button from '../button/Button';
 
 const Header = () => {
-    const {user, onClose} = useTelegram();
+    const { user, onClose } = useTelegram();
+    const navigate = useNavigate(); // Инициализируем navigate
+
+    const handleNavigateToForm = () => {
+        navigate('/form'); // Переход на страницу формы
+    };
 
     return (
         <div className={'header'}>
@@ -12,6 +18,7 @@ const Header = () => {
             <span className={'username'}>
                 {user?.username}
             </span>
+            <Button onClick={handleNavigateToForm}>Перейти к форме</Button> {/* Кнопка для перехода */}
         </div>
     );
 };
